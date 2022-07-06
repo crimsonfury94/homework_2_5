871fca992be96d97b7f3cac5d4dbf342f5b75c74
Dmitry <leon1861@yandex.ru>
2022-07-06 21:08:51 +0700

    ДЗ 2.13

---
+++ src/test/java/com/example/homework25/homework_2_5/impl/EmployeeServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.example.homework25.homework_2_5.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+
+    @Test
+    void shouldReturnResultWhenAddSameEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void getEmployee() {
+    }
+}
